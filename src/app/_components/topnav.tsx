ab74ab7095f2e3eed3152fa3540b33df095b4628
001wabi <adityasupare2003@gmail.com>
2024-10-19 19:58:27 +0530

    auth done

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,18 @@
+import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
+
+export function TopNav(){
+    return(
+      <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+        <div>CineSync</div>
+
+        <div>
+            <SignedOut>
+                <SignInButton/>
+            </SignedOut>
+            <SignedIn>
+                <UserButton/>   
+            </SignedIn>
+        </div>
+      </nav>
+    )
+  }
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,35 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-
-import { env } from "~/env";
+import { posts } from "./schema";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
+
+const sql = postgres({
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    user: process.env.POSTGRES_USER,    
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,    
+    ssl: 'require'
+  });
+  
+
+export const db = drizzle(sql, { schema });
+
+export const getUsers = async () => {
+  const selectResult = await db.select().from(posts);
+  console.log("Results", selectResult);
+  return selectResult;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+export type NewUser = typeof posts.$inferInsert;
 
-export const db = drizzle(conn, { schema });
+export const insertUser = async (user: NewUser) => {
+  return db.insert(posts).values(user).returning();
+};
+
+export const getUsers2 = async () => {
+  const result = await db.query.posts.findMany();
+  return result;
+};
+
